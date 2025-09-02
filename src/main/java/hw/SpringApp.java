72681f3f74e409dc37c95a4e5c6577e4782c9784
Artur Gafarov <am.gafarov@vtb.ru>
2025-09-02 14:10:35 +0300

    home work 5. Переезд на spring-data-jpa

--- src/main/java/hw/SpringApp.java
+++ src/main/java/hw/SpringApp.java
@@ -1,35 +1,11 @@
 package hw;
 
-import hw.user.User;
-import hw.user.UserService;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@ComponentScan
+@SpringBootApplication
 public class SpringApp {
     public static void main(String[] args) {
-        var context = new AnnotationConfigApplicationContext(SpringApp.class);
-        UserService userService = context.getBean(UserService.class);
-
-        userService.deleteAll();
-
-        User alex = userService.create("Александр");
-        System.out.println(userService.getAll());
-
-        User egor = userService.create("Егор");
-        System.out.println(userService.getAll());
-
-        User updatedUser = userService.update(egor.getId(), "Не Егор");
-        System.out.println(userService.getById(updatedUser.getId()));
-
-        userService.delete(alex.getId());
-        System.out.println(userService.getAll());
-
-        try {
-            userService.getById(alex.getId());
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-
+        SpringApplication.run(SpringApp.class, args);
     }
 }
---
+++ src/main/java/hw/entity/User.java
@@ -0,0 +1,25 @@
+package hw.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "users_id_seq")
+    @SequenceGenerator(name = "users_id_seq", sequenceName = "users_id_seq", allocationSize = 1)
+    @Column(nullable = false)
+    private Long id;
+
+    @Column(name = "username", nullable = false)
+    private String username;
+}
